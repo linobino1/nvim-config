e3da79501cd686ddc027563df4a2a906042a95c2
Leo <leo.hilsheimer@gmail.com>
2025-04-29 17:46:29 +0200

    set tab size to 4

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -26,3 +26,6 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- tab size 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
